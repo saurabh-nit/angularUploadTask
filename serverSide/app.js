5ae77a9a96d90d34a9776189b1f6bbf0616d0487
saurabh-nit <skmishra.nitdgp@gmail.com>
2017-05-29 18:14:46 +0530

    modified app

--- serverSide/app.js
+++ serverSide/app.js
@@ -1,40 +1,40 @@
 var express = require('express');
-var multer  = require('multer');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var index = require('./routes/index');
 var app = express();
+var multer  = require('multer');
+var bodyParser = require('body-parser');
 
+/************** WE ARE USING SAME EXPRESS SERVER FOR BOTH client and server ************/
 app.use(express.static('../clientSide'));
 app.use(bodyParser.json());
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+/**************  FILE UPLOAD PACKAGE FOR NODEJS *******************************/
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename : function (req, file, cb) {
+    cb(null, file.originalname)
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+var upload = multer({ storage: storage });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+/************** API URL TO UPLOAD THE FILE *************************************/
+app.post('/upload', upload.any(), function(req,res){
+  var data = req.files;
+  console.log(data);
+  console.log("At server side");
+  res.json({file : data, error : false});
+});
+
+
+
+/*************** PORT & SERVER INITIALIZATION **********************************/
+app.listen('3000', function(){
+    console.log('Server is running on 3000...');
 });
 
 module.exports = app;
